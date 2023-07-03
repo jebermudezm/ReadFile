2174f3ddfc05972193b14ae5b4237c9b012f7d48
Joaquin Bermudez <jeberme@hotmail.com>
2023-07-03 12:40:19 -0500

    Add project files.

---
+++ ReadFile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+            FileInfo fileInfo = new FileInfo($"{path}\\json.txt");
+            string textFile;
+            using (var streamReaderFile = new StreamReader(fileInfo.FullName))
+            {
+                textFile = streamReaderFile.ReadToEnd();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
